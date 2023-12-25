051c91d41d7b80108ebdd1bf9f7148acba8adf1b
Baek <woonn97@gmail.com>
2023-12-25 13:04:30 +0900

    [✨feat] : blog 초기 목록 구현

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { CoProvider } from "@co-design/core";
+import type { AppProps } from "next/app";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <CoProvider withNormalizeCSS withGlobalStyles>
+        <Component {...pageProps} />
+      </CoProvider>
+    </ApolloProvider>
+  );
 }
